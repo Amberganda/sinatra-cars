85da9c6cd99b67d5675f4977efd5ea4f2777b96c
Amber Taranto <ambzzz@gmail.com>
2019-10-13 19:33:17 -0700

    got the users/signup to load

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ require "./config/environment"
 require "./app/models/user"
 
 class ApplicationController < Sinatra::Base
-
-    get "/" do
-        "hello world"
+    register Sinatra::ActiveRecordExtension
+    set :views, Proc.new { File.join(root, "../views/") }
+  
+    configure do
+      enable :sessions
+      set :session_secret, "secret"
     end
     
+    get "/users/signup" do
+        erb:'user/signup'
+    end
+
+
 end
