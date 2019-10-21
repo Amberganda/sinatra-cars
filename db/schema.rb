d0b6e93fb6af464e646592d9848bfe899d81d529
Amber Taranto <ambzzz@gmail.com>
2019-10-20 19:46:43 -0700

    updated schema to associate index of cars on user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191014020940) do
+ActiveRecord::Schema.define(version: 20191021023848) do
 
   create_table "cars", force: :cascade do |t|
     t.string "license_plate_number"
     t.string "make"
     t.string "model"
     t.integer "year"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
